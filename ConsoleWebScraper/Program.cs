bdda07f91be717f0d2a71181d5fa265b95fa04a1
tsinkunas <tsinkunas@gmail.com>
2020-12-21 06:28:32 +0200

    Add project files.

---
+++ ConsoleWebScraper/Program.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleWebScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var responce = await httpClient.GetAsync("https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos");
+
+            var responceBody = await responce.Content.ReadAsStringAsync();
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(responceBody);
+
+            var links = htmlDoc.DocumentNode.Descendants("a")
+                .Where(node => node.GetAttributeValue("itemprop","").Contains("title")).ToList();
+            var title = links.Select(l => l.InnerText);
+            foreach (var item in title)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
